8a44e015f25e51ac66aae8e7aa5cfe1baab530aa
David Campos <davidce0107@gmail.com>
2025-05-06 20:14:20 -0600

    Cambio en el texto de prueba

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -4,7 +4,8 @@ export default function Titulo({titulo}) {
   return (
     <>
         <div>{titulo}</div>
-        <p>Este es un texto de prueba para el uso de GIT</p>
+        <p>Este es un texto de prueba para el uso de GIT.</p>
+        <p>Este es un texto de prueba para el uso de GIT.</p>
     </>
     
   )
